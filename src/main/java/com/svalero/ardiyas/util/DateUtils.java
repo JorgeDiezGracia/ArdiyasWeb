96185265c2e4515e5219f6626163a7449b465389
JorgeDiezGracia <133242180+JorgeDiezGracia@users.noreply.github.com>
2024-08-20 19:51:00 +0200

    new method validate

--- src/main/java/com/svalero/ardiyas/util/DateUtils.java
+++ src/main/java/com/svalero/ardiyas/util/DateUtils.java
@@ -3,6 +3,9 @@ package com.svalero.ardiyas.util;
 import java.sql.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 import static com.svalero.ardiyas.util.Constants.DATE_PATTERN;
 
@@ -15,6 +18,16 @@ public class DateUtils {
 
     public static Date parse(String date) throws ParseException {
         SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_PATTERN);
-       return new Date(dateFormat.parse(date).getTime());
+        return new Date(dateFormat.parse(date).getTime());
+    }
+
+    public static boolean validate(String date) throws ParseException {
+        try {
+            DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern(DATE_PATTERN);
+            LocalDate.parse(date, dateFormat);
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+        return true;
     }
 }
